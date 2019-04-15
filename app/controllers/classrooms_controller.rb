2b2af0c2923de88ddde62f94cc2fb337ee5d3b9e
Rafael Batista <rafaelb24@yahoo.com>
2019-04-15 22:43:57 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,15 @@
 class ClassroomsController < ApplicationController
   def show
+    binding.pry
     @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
+    @students = @classroom.students
   end
 
   def index
     @classrooms = Classroom.all
   end
 end
+
+#<% @students.each do |student| %>
+ # <%= render partial: 'students/student', locals: {student: student} %>
+#<% end %>
